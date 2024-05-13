659688c813c24d2131e7c6609c4a1756df8dd679
Nitish76kumar <nitish11dhoni@gmail.com>
2024-05-13 14:28:00 +0530

    up

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,7 @@ const router = require("./routes");
 
 const app = express();
 app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  })
+  cors()
 );
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
@@ -22,6 +19,6 @@ const port = process.env.PORT || 8000;
 connectDB().then(() => {
   app.listen(port, () => {
     console.log("connnect to DB");
-    console.log("Server is running " + PORT);
+    console.log("Server is running " + port);
   });
 });
